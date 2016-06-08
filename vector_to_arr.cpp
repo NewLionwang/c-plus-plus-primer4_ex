6223ab6c7e332408afac3e1e76600f89b8bd037c
wangxx <wangxiaoxian@ebupt.com>
2016-06-08 19:17:41 +0800

    vector复制给动态数组，并遍历动态数组

---
+++ vector_to_arr.cpp
@@ -0,0 +1,30 @@
+/*************************************************************************
+	> File Name: vector_to_arr.cpp
+	> Author: wangxiaoxian
+	> Mail: 634897019@qq.com 
+	> Created Time: Wed 08 Jun 2016 06:49:29 PM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int> ivec;
+    int ival;
+    cout << "Enter numbers:(ctrl+d to end)" << endl;
+    while(cin >> ival)
+        ivec.push_back(ival);
+    int *pia  = new int(ivec.size());
+
+    int *tp = pia;
+    for(vector<int>::iterator iter = ivec.begin();
+            iter != ivec.end(); ++iter,++tp)
+    {
+        *tp = *iter;
+        cout << *tp << " " << endl;
+    }
+    delete [] pia;
+    return 0;
+}
